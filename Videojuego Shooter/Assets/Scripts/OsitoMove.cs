3f6032d0b75fd4690cb06b2254c0f19463925533
Jesus-NG <jesus.ng119@gmail.com>
2023-03-28 11:51:15 -0600

    Movimiento

--- Videojuego Shooter/Assets/Scripts/OsitoMove.cs
+++ Videojuego Shooter/Assets/Scripts/OsitoMove.cs
@@ -23,5 +23,35 @@ public class OsitoMove : MonoBehaviour
 
         if (Horizontal < 0.0f) transform.localScale = new Vector3(-0.25f, 0.25f, 0.25f);
         else if (Horizontal > 0.0f) transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+
+        Animator.SetBool("Running", Horizontal != 0.0f);
+        Animator.SetBool("Grounded", Grounded);
+
+        Debug.DrawRay(transform.position, Vector3.down * 1.5f, Color.red);
+
+        if(Physics2D.Raycast(transform.position, Vector3.down, 1.5f))
+        {
+            Grounded = true;
+        }
+        else Grounded = false;
+
+
+        if (Input.GetKeyDown(KeyCode.W) && Grounded)
+        {
+            Jump();
+        }
+
     }
+
+
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
+    }
+
+    private void FixedUpdate() 
+    {
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
+    }
+
 }
